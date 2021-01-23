f80aa5c983de44aa1346a8b47c18bfbf610b88dd
Minji Kim <52568892+minji-mia@users.noreply.github.com>
2021-01-23 00:00:55 -0600

    DB function: create tables

--- db_functions.py
+++ db_functions.py
@@ -4,3 +4,7 @@ import sqlite3
 conn = sqlite3.connect('data.db')
 c = conn.cursor()
 
+# Functions
+def create_table():
+    c.execute('CREATE TABLE IF NOT EXISTS blogtable(author TEXT, title TEXT, article TEXT, postdate DATE)')
+
