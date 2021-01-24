47896ee1e7a044f73c1df27c1170168269c9f781
Minji Kim <52568892+minji-mia@users.noreply.github.com>
2021-01-24 09:38:46 -0600

    DB functions: add data

--- db_functions.py
+++ db_functions.py
@@ -8,3 +8,6 @@ c = conn.cursor()
 def create_table():
     c.execute('CREATE TABLE IF NOT EXISTS blogtable(author TEXT, title TEXT, article TEXT, postdate DATE)')
 
+def add_data(author, title, article, postdate):
+    c.execute('INSERT INTO blogtable(author, title, article, postdate) VALUES(?,?,?,?)', (author, title, article, postdate))
+    conn.commit()
